f0165f82014efccf8f280d1e8e30f7dc6f58e451
Royprecious <djokoroy50@gmail.com>
2024-10-11 18:46:01 +0300

    still working on it

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-let dessertObjectData=  [
+let dessertObjectData = [
     {
-       "image": {
+        "image": {
             "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
             "mobile": "./assets/images/image-waffle-mobile.jpg",
             "tablet": "./assets/images/image-waffle-tablet.jpg",
             "desktop": "./assets/images/image-waffle-desktop.jpg"
-       },
-       "name": "Waffle with Berries",
-       "category": "Waffle",
-       "price": 6.50,
-       "id": 1,
+        },
+        "name": "Waffle with Berries",
+        "category": "Waffle",
+        "price": 6.50,
+        "id": 1,
     },
     {
         "image": {
@@ -23,8 +23,8 @@ let dessertObjectData=  [
         "category": "Crème Brûlée",
         "price": 7.00,
         "id": 2,
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
             "mobile": "./assets/images/image-macaron-mobile.jpg",
@@ -35,8 +35,8 @@ let dessertObjectData=  [
         "category": "Macaron",
         "price": 8.00,
         "id": 3,
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
             "mobile": "./assets/images/image-tiramisu-mobile.jpg",
@@ -48,8 +48,8 @@ let dessertObjectData=  [
         "price": 5.50,
         "id": 4,
 
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
             "mobile": "./assets/images/image-baklava-mobile.jpg",
@@ -60,8 +60,8 @@ let dessertObjectData=  [
         "category": "Baklava",
         "price": 4.00,
         "id": 5,
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
             "mobile": "./assets/images/image-meringue-mobile.jpg",
@@ -72,8 +72,8 @@ let dessertObjectData=  [
         "category": "Pie",
         "price": 5.00,
         "id": 6,
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
             "mobile": "./assets/images/image-cake-mobile.jpg",
@@ -85,8 +85,8 @@ let dessertObjectData=  [
         "price": 4.50,
         "id": 7,
 
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
             "mobile": "./assets/images/image-brownie-mobile.jpg",
@@ -97,8 +97,8 @@ let dessertObjectData=  [
         "category": "Brownie",
         "price": 4.50,
         "id": 8,
-     },
-     {
+    },
+    {
         "image": {
             "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
             "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
@@ -110,103 +110,185 @@ let dessertObjectData=  [
         "price": 6.50,
         "id": 9,
 
-     }
+    },
+
 ]
 
 
 
 
-function loadData(){
+function loadData() {
 
     let dessertContainer = document.getElementById("dessert-container");
 
     dessertObjectData.forEach((dessertObject) => {
 
 
-let newDessertBoxItem = document.createElement("div");
-newDessertBoxItem.classList.add ("dessert-item-box");
+        let newDessertBoxItem = document.createElement("div");
+        newDessertBoxItem.classList.add("dessert-item-box");
 
-let newDessertImage = document.createElement("img");
-newDessertImage.setAttribute("src", dessertObject.image.desktop);
+        let newDessertImage = document.createElement("img");
+        newDessertImage.setAttribute("src", dessertObject.image.desktop);
 
-let dessertTitle = document.createElement("h4");
-let dessertTitleTextnode = document.createTextNode(dessertObject.name);
-dessertTitle.appendChild(dessertTitleTextnode);
+        let dessertTitle = document.createElement("h4");
+        let dessertTitleTextnode = document.createTextNode(dessertObject.name);
+        dessertTitle.appendChild(dessertTitleTextnode);
 
-let dessertCategory = document.createElement("p");
-let dessertCategoryTextNode = document.createTextNode(dessertObject.category);
-dessertCategory.appendChild(dessertCategoryTextNode);
-
-
-let dessertPrice = document.createElement("p");
-dessertPrice.classList.add("priceColor");
-let dessertPriceTextNode = document.createTextNode("$" + dessertObject.price.toFixed(2));
-dessertPrice.appendChild(dessertPriceTextNode);
-
-let newButton = document.createElement("button");
-let quantity = 0;
-newButton.addEventListener("click", ()=> {
-
-    // const cartItem = dessertObject.find(item=>item.id === id);
-    // console.log(cartItem);
-
-    
-    let secondNewButton = document.createElement("button");
-    secondNewButton.classList.add("secondButton");
-
-    let index = document.createElement("p");
-
-    let indexTextNode = document.createTextNode(quantity);
-    index.appendChild(indexTextNode);
-
-    let incrementIcon = document.createElement("img");
-    incrementIcon.classList.add("increAndDecre-btn");
-    incrementIcon.addEventListener("click", ()=> {
-        
-         quantity++;
-       let count =  index.textContent = quantity;   
-     // cart 1st step
-     let addTOcart = document.getElementById("add-to-cart"); 
-      let totalCartItem = document.getElementById("cart-item-total");
-      totalCartItem.textContent = `your cart   ( ${count } ) `;
+        let dessertCategory = document.createElement("p");
+        let dessertCategoryTextNode = document.createTextNode(dessertObject.category);
+        dessertCategory.appendChild(dessertCategoryTextNode);
 
 
-     let cartItem = document.createElement("p");
-     let cartItemTextNode = document.createTextNode(dessertObject.name);
-     cartItem.appendChild(cartItemTextNode);
+        let dessertPrice = document.createElement("p");
+        dessertPrice.classList.add("priceColor");
+        let dessertPriceTextNode = document.createTextNode("$" + dessertObject.price.toFixed(2));
+        dessertPrice.appendChild(dessertPriceTextNode);
 
-     let currentItem = document.createElement("p");
-     let currentItemTextNode = document.createTextNode(count);
-     currentItem.appendChild(currentItemTextNode);
-     addTOcart.append(cartItem, currentItem, dessertObject.price * count);
+        let newButton = document.createElement("button");
+        let quantity = 0;
 
-    })
-    incrementIcon.setAttribute("src", "assets/images/icon-increment-quantity.svg");
+        newButton.addEventListener("click", async () => {
 
-    let decrementIcon = document.createElement("img");
-    decrementIcon.classList.add("increAndDecre-btn");
-    decrementIcon.addEventListener("click", ()=>{
-        if(quantity > 0){
-            quantity--;
-            index.textContent = quantity;
-        }
-    });
-    decrementIcon.setAttribute("src", "assets/images/icon-decrement-quantity.svg");
-    secondNewButton.append(decrementIcon,index,incrementIcon);
-    newButton.replaceWith(secondNewButton);
+               //This function helps to store your datas in the cart(located in the localStorage);
+
+                //Start of the function to  store data to the localStorage
 
 
-   
-    
-})
-let buttonImage = document.createElement("img");
-buttonImage.setAttribute("src", "assets/images/icon-add-to-cart.svg");
-let addCartText = document.createElement("p");
-let addCartTextNode = document.createTextNode("Add to cart");
-addCartText.appendChild(addCartTextNode);
-newButton.append(buttonImage,addCartText);
-newDessertBoxItem.append(newDessertImage,dessertCategory,dessertTitle,dessertPrice,newButton);
-dessertContainer.appendChild(newDessertBoxItem);
+                async function storeDataInLocalStorage() {
+
+                    //This line gets the data from the cart and append the incomming data to it before storing in the DB
+                    let myCart = await JSON.parse(localStorage.getItem('cart')) || [];
+
+                    const dataExist = await myCart.find(data => data.id === dessertObject.id);
+                     
+                    //verifying of the data exist in the cart be fore adding a new one
+                    if (!dataExist) {
+                        myCart.push(dessertObject);
+                        localStorage.setItem('cart', JSON.stringify(myCart));
+                        console.log('data was sucessfully added to cart');
+
+                        const info = {
+                            type: true,
+                            message: 'data was sucessfully added to cart',
+                            
+                        }
+                        return info;
+                    }
+
+                    //creating my return type
+                    const info = {
+                        type: false,
+                        message: 'data exist in the cart',
+                    }
+
+
+                    return info;
+                }
+
+
+
+                
+                //declaring my function and storing its return in this variable
+                const addingToThisCart = await storeDataInLocalStorage();
+                 
+                //making sure the code does not proceed if the return from the above function is false
+                if (addingToThisCart.type === false) {
+                    console.log(addingToThisCart.message);
+                    return;
+                }
+
+
+
+
+            let secondNewButton = document.createElement("button");
+            secondNewButton.classList.add("secondButton");
+
+            let index = document.createElement("p");
+
+            let indexTextNode = document.createTextNode(quantity);
+            index.appendChild(indexTextNode);
+
+            let incrementIcon = document.createElement("img");
+            incrementIcon.classList.add("increAndDecre-btn");
+
+            incrementIcon.addEventListener("click", async () => {
+                quantity++;
+                let count = index.textContent = quantity;
+             
+                      let dataInCart = await JSON.parse(localStorage.getItem('cart')) || [];
+                          
+                      const dataExistInCart = await dataInCart.find(item=> item.id === dessertObject.id);
+                
+                      if(dataExistInCart){
+                          dataExistInCart.quantity = quantity + 1;
+                          await dataInCart.push(dataExistInCart);
+                        await localStorage.setItem('cart', JSON.stringify(dataInCart));
+                          console.log(await JSON.parse(localStorage.getItem('cart')));
+                           return;
+                      }
+
+                      
+
+                    // console.log(addingToThisCart.info);
+                    // console.log(quantity++);
+                
+                 
+
+                
+                // cart 1st step
+                let addTOcart = document.getElementById("add-to-cart");
+                let totalCartItem = document.getElementById("cart-item-total");
+                totalCartItem.textContent = `your cart   ( ${count} ) `;
+
+
+
+
+
+
+             
+
+
+
+                let cartItem = document.createElement("p");
+                let cartItemTextNode = document.createTextNode(dessertObject.name);
+                cartItem.appendChild(cartItemTextNode);
+
+                let currentItem = document.createElement("p");
+                let currentItemTextNode = document.createTextNode(count);
+                currentItem.appendChild(currentItemTextNode);
+                addTOcart.append(cartItem, currentItem, dessertObject.price * count, 'hgh');
+
+            })
+
+            //end of the function to store data to the localStorage
+
+
+            incrementIcon.setAttribute("src", "assets/images/icon-increment-quantity.svg");
+
+            let decrementIcon = document.createElement("img");
+            decrementIcon.classList.add("increAndDecre-btn");
+            decrementIcon.addEventListener("click", () => {
+                if (quantity > 0) {
+                    quantity--;
+                    index.textContent = quantity;
+                }
+            });
+            decrementIcon.setAttribute("src", "assets/images/icon-decrement-quantity.svg");
+            secondNewButton.append(decrementIcon, index, incrementIcon);
+            newButton.replaceWith(secondNewButton);
+
+
+
+
+        })
+        let buttonImage = document.createElement("img");
+        buttonImage.setAttribute("src", "assets/images/icon-add-to-cart.svg");
+        let addCartText = document.createElement("p");
+        let addCartTextNode = document.createTextNode("Add to cart");
+        addCartText.appendChild(addCartTextNode);
+        newButton.append(buttonImage, addCartText);
+        newDessertBoxItem.append(newDessertImage, dessertCategory, dessertTitle, dessertPrice, newButton);
+        dessertContainer.appendChild(newDessertBoxItem);
 
     });
 }
